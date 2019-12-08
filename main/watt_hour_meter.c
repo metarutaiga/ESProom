2b0c2681f747305fdd66daf97f11520ac14df0a0
TAiGA <>
2019-12-08 23:28:54 +0800

    Prototype

--- main/watt_hour_meter.c
+++ main/watt_hour_meter.c
@@ -12,9 +12,11 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
+#include "lwip/apps/sntp.h"
 
 #include "esp_log.h"
 #include "esp_system.h"
+#include "esp_timer.h"
 #include "nvs_flash.h"
 #include "app_wifi.h"
 
@@ -50,7 +52,62 @@ static const char * const CONFIG_FORM_HOUR[24] =
     CONFIG_FORM_HOUR_23,
 };
 
-static const char * const TAG = "HTTP_CLIENT";
+static const char * const TAG = "WATT-HOUR METER";
+
+static void initialize_sntp(void)
+{
+    ESP_LOGI(TAG, "Initializing SNTP");
+    sntp_setoperatingmode(SNTP_OPMODE_POLL);
+    sntp_setservername(0, "tw.pool.ntp.org");
+    sntp_init();
+}
+
+static void obtain_time(void)
+{
+    app_wifi_wait_connected();
+    initialize_sntp();
+
+    // wait for time to be set
+    time_t now = 0;
+    struct tm timeinfo = { 0 };
+    int retry = 0;
+    const int retry_count = 10;
+
+    while (timeinfo.tm_year < (2016 - 1900) && ++retry < retry_count) {
+        ESP_LOGI(TAG, "Waiting for system time to be set... (%d/%d)", retry, retry_count);
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+        time(&now);
+        localtime_r(&now, &timeinfo);
+    }
+}
+
+static void sntp(void *parameter)
+{
+    time_t now;
+    struct tm timeinfo;
+    char strftime_buf[64];
+
+    while (1) {
+
+        // update 'now' variable with current time
+        time(&now);
+        localtime_r(&now, &timeinfo);
+
+        if (timeinfo.tm_year < (2016 - 1900)) {
+            ESP_LOGE(TAG, "The current date/time error");
+
+            obtain_time();
+            setenv("TZ", "GMT-8", 1);
+            tzset();
+        } else {
+            strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);
+            ESP_LOGI(TAG, "The current date/time in Taipei is: %s", strftime_buf);
+            break;
+        }
+    }
+
+    vTaskDelete(NULL);
+}
 
 esp_err_t _http_event_handler(esp_http_client_event_t *evt)
 {
@@ -137,18 +194,48 @@ static void pulse_web(void *parameter)
 
 static void pulse_log(void *parameter)
 {
-    ESP_LOGI(TAG, "pulse : %d", PULSE_PER_HOUR[0]);
+    time_t now;
+    struct tm timeinfo;
+
+    time(&now);
+    localtime_r(&now, &timeinfo);
+
+    ESP_LOGI(TAG, "pulse : %d (%d:%d:%d)", PULSE_PER_HOUR[timeinfo.tm_hour], timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec);
     vTaskDelete(NULL);
 }
 
 static void pulse(void *parameter)
 {
-    PULSE_PER_HOUR[0]++;
+    static int last_hour = -1;
+    static int64_t last_time = 0;
+    int64_t pulse_time = esp_timer_get_time();
+    int64_t delta_time = pulse_time - last_time;
+    time_t now;
+    struct tm timeinfo;
+
+    if (delta_time < 100 * 1000)
+       return;
+    last_time = pulse_time;
+
+    time(&now);
+    localtime_r(&now, &timeinfo);
+    if (timeinfo.tm_year < (2016 - 1900))
+        return;
+
+    PULSE_PER_HOUR[timeinfo.tm_hour]++;
     xTaskCreate(&pulse_log, "pulse_log", 8192, NULL, 5, NULL);
 
-    if (PULSE_PER_HOUR[0] == 10) {
+    if (last_hour == timeinfo.tm_hour)
+        return;
+    if (last_hour != -1) {
         xTaskCreate(&pulse_web, "pulse_web", 8192, NULL, 5, NULL);
+        if (last_hour == 23) {
+            for (int i = 0; i < 24; ++i)
+                PULSE_PER_HOUR[i] = 0;
+            PULSE_PER_HOUR[timeinfo.tm_hour]++;
+        }
     }
+    last_hour = timeinfo.tm_hour;
 }
 
 void app_main()
@@ -161,8 +248,11 @@ void app_main()
     ESP_ERROR_CHECK(ret);
     app_wifi_initialise();
 
-    gpio_install_isr_service(0);
-    gpio_set_pull_mode(GPIO_NUM_2, GPIO_PULLUP_ONLY);
+    xTaskCreate(sntp, "sntp", 2048, NULL, 10, NULL);
+
+    gpio_set_direction(GPIO_NUM_2, GPIO_MODE_INPUT);
     gpio_set_intr_type(GPIO_NUM_2, GPIO_INTR_NEGEDGE);
+    gpio_set_pull_mode(GPIO_NUM_2, GPIO_PULLUP_ONLY);
+    gpio_install_isr_service(0);
     gpio_isr_handler_add(GPIO_NUM_2, pulse, NULL);
 }
